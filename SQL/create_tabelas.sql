ec336859cee2863dbec3c8c31798a115f9c9bff4
Pigas22 <tholzc@gmail.com>
2024-10-05 23:10:25 -0300

    Correção da Classe Database e Adição de arquivos SQL.

---
+++ SQL/create_tabelas.sql
@@ -0,0 +1,88 @@
+CREATE TABLE endereco(
+id_endereco INTEGER PRIMARY KEY NOT NULL,
+numero integer NOT NULL,
+rua VARCHAR (30) NOT NULL,
+bairro VARCHAR  (30),
+cidade VARCHAR (30) NOT NULL,
+uf VARCHAR(2) NOT NULL); 
+
+CREATE TABLE pessoa(
+id_pessoa integer PRIMARY KEY NOT NULL,
+nome_pessoa VARCHAR (100) NOT NULL,
+cpf VARCHAR(14) NOT NULL,
+id_endereco INT NOT NULL,
+FOREIGN KEY (id_endereco) REFERENCES endereco(id_endereco));
+
+CREATE TABLE franqueadoras(
+id_franqueadora integer PRIMARY KEY  NOT NULL,
+nome VARCHAR (30) NOT NULL,
+qtd_franquia integer NOT NULL ,
+id_endereco integer NOT NULL,
+FOREIGN KEY(id_endereco) REFERENCES endereco(id_endereco));
+
+CREATE TABLE cinema (
+id_cinema integer PRIMARY KEY NOT NULL,
+nome_cinema VARCHAR (30) NOT NULL ,
+id_franqueadora INT NOT NULL ,
+id_endereco INT NOT NULL ,
+FOREIGN KEY(id_endereco) REFERENCES endereco(id_endereco),
+FOREIGN KEY(id_franqueadora) REFERENCES franqueadoras(id_franqueadora));
+
+CREATE TABLE filme (
+id_filme INT PRIMARY KEY NOT NULL, 
+nome_filme VARCHAR (30) NOT NULL, 
+preco NUMERIC (5,2) NOT NULL);
+
+CREATE TABLE secao(
+id_secao INT PRIMARY KEY NOT NULL ,
+horario TIMESTAMP NOT NULL ,
+id_cinema INT NOT NULL,
+id_filme INT NOT NULL ,
+qtd_assentos INT NOT NULL,
+FOREIGN KEY(id_cinema) REFERENCES cinema(id_cinema),
+FOREIGN KEY(id_filme) REFERENCES filme(id_filme));
+
+CREATE TABLE cliente (
+id_cliente INT PRIMARY KEY NOT NULL , 
+id_pessoa INT NOT NULL , 
+FOREIGN KEY (id_pessoa) REFERENCES pessoa(id_pessoa));
+
+CREATE TABLE vendedor (
+id_vendedor INT PRIMARY KEY NOT NULL ,
+id_pessoa INT NOT NULL ,
+qtd_vendas INT NOT NULL,
+FOREIGN KEY (id_pessoa) REFERENCES pessoa(id_pessoa));
+
+CREATE TABLE venda(
+id_secao INT NOT NULL , 
+id_vendedor INT NOT NULL ,
+id_cliente INT NOT NULL, 
+PRIMARY KEY (id_secao,id_vendedor,id_cliente),
+CONSTRAINT  fk_secao FOREIGN KEY (id_secao) REFERENCEs secao(id_secao),
+CONSTRAINT  fk_vendedor FOREIGN KEY (id_vendedor) REFERENCEs vendedor (id_vendedor),
+CONSTRAINT  fk_cliente FOREIGN KEY (id_cliente) REFERENCEs cliente (id_cliente));
+
+CREATE TABLE metas(
+id_meta INT PRIMARY KEY NOT NULL ,
+descricao VARCHAR (100) NOT NULL,
+remuneracao NUMERIC(3,2) NOT NULL );
+
+CREATE TABLE meta_vendedor(
+id_meta INT NOT NULL, 
+id_vendedor INT NOT NULL ,
+meta_batida BOOLEAN NOT NULL,
+PRIMARY KEY (id_meta,id_vendedor),
+CONSTRAINT fk_meta FOREIGN KEY (id_meta) REFERENCES metas(id_meta),
+CONSTRAINT fk_vendedor FOREIGN KEY(id_vendedor) REFERENCES vendedor (id_vendedor));
+
+CREATE TABLE contato (
+id_contato INT PRIMARY KEY NOT NULL , 
+tipo_contato VARCHAR (15) NOT NULL, 
+contato VARCHAR (40) NOT NULL);
+
+CREATE TABLE pessoa_contato(
+id_pessoa INT NOT NULL , 
+id_contato INT NOT NULL, 
+PRIMARY KEY (id_pessoa,id_contato),
+CONSTRAINT fk_pessoa FOREIGN KEY (id_pessoa) REFERENCES pessoa(id_pessoa),
+CONSTRAINT fk_contato FOREIGN KEY (id_contato) REFERENCES contato(id_contato));
---
+++ projeto-java/src/conexion/Database.java
@@ -0,0 +1,171 @@
+package conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import utils.*;
+
+public class Database {
+    // Classes auxiliares
+    private static MenuFormatter menu = new MenuFormatter();
+    private static Arquivo arq = new Arquivo();
+
+    // Váriaveis referentes à criação do banco da Aplicação
+    private static final String PORTA_SERVICO = "5432";
+    private static final String URL_POSTGRES = "jdbc:postgresql://localhost:" + PORTA_SERVICO + "/postgres";
+    private static final String USER_POSTGRES = "postgres";
+    private static final String PASSWORD_POSTGRES = "postgres";
+    private static final String NOME_DATABASE = "ger_cinema_db";
+    
+    // Váriaveis do Arquivo SQL Inicial
+    private static final String ARQ_CEATE = "create_tabelas.sql";
+    private static final String ARQ_DROP = "drop_tabelas.sql";
+    private static final String NOME_PASTA_SQL = "SQL";
+    private static final String CAMINHO_PASTA_SQL = arq.procuraPasta(NOME_PASTA_SQL);
+
+    // Váriaveis referentes ao banco da Aplicação
+    private static final String URL_DB = "jdbc:postgresql://localhost:" + PORTA_SERVICO + "/" + NOME_DATABASE;
+
+    // Váriavel para checar dados prévios
+    private static final String CHECK_DATA_SQL = "SELECT COUNT(*) AS qtd_tabelas FROM information_schema.tables WHERE table_schema = 'public';";
+
+    // Método para criar o banco informado na variável NOME_DATABASE
+    public static boolean criarDatabase() {
+        String createDatabaseSQL = "CREATE DATABASE " + NOME_DATABASE;
+
+        try (Connection conn = DriverManager.getConnection(URL_POSTGRES, USER_POSTGRES, PASSWORD_POSTGRES);
+            Statement stmt = conn.createStatement()) {
+
+            String bancoExiste = "";
+            ResultSet rs = stmt.executeQuery("SELECT CASE COUNT(*) WHEN 0 THEN 'N' WHEN 1 THEN 'S' END AS resultado FROM pg_database WHERE datname = '" + NOME_DATABASE + "'");
+            while (rs.next()) {
+                bancoExiste = rs.getString("resultado");
+
+            }   
+
+            if (bancoExiste.strip().equals("N")) {
+                stmt.executeUpdate(createDatabaseSQL);
+                menu.msgTerminalINFO("Banco de dados '" + NOME_DATABASE + "' criado com sucesso.");
+                return true;
+
+            } else {
+                menu.msgTerminalINFO("Banco de dados já existente.");
+                return false;
+
+            }
+
+
+        } catch (SQLException e) {
+            menu.msgTerminalERROR("Erro ao criar o banco de dados: " + e.getMessage());
+            return false;
+        }
+    }
+
+    // Método para dropar o banco informado na variável NOME_DATABASE
+    public static void droparDatabase() {
+        String createDatabaseSQL = "DROP DATABASE IF EXISTS " + NOME_DATABASE;
+
+        try (Connection conn = DriverManager.getConnection(URL_POSTGRES, USER_POSTGRES, PASSWORD_POSTGRES);
+            Statement stmt = conn.createStatement()) {
+
+            stmt.executeUpdate(createDatabaseSQL);
+            menu.msgTerminalINFO("Banco de dados '" + NOME_DATABASE + "' dropado com sucesso.");
+
+        } catch (SQLException e) {
+            menu.msgTerminalERROR("Erro ao dropar o banco de dados: " + e.getMessage());
+        }
+    }
+
+    // Método para conectar no BANCO
+    public static Connection conectar() {
+        Connection conexao = null;
+        try {
+            conexao = DriverManager.getConnection(URL_DB, USER_POSTGRES, PASSWORD_POSTGRES);
+
+        } catch (SQLException e) {
+            menu.msgTerminalERROR(e.getMessage());
+        }
+        return conexao;
+    }
+
+    // Método para inserir os dados inciais do BANCO
+    public static void inicializarDatabase() {
+        String comandoSQL;
+
+        try (Connection conexao = conectar();
+             Statement stmt = conexao.createStatement()) {
+
+            // Verifica a quantidade de Tabelas
+            ResultSet rs = stmt.executeQuery(CHECK_DATA_SQL);
+            int num = 0;
+            while (rs.next()) {
+                num = rs.getInt("qtd_tabelas");
+
+            }
+
+            // Caso tenha alguma tabela no schema, todas elas serão excluídas
+            if (num > 0) {
+                excluirTabelas();
+            } 
+
+            // Adiciona as tabelas informadas no arquivo create_tabelas.sql
+            comandoSQL = arq.lerSQL(CAMINHO_PASTA_SQL + "\\" + ARQ_CEATE);
+            executarSQL(comandoSQL);
+            menu.msgTerminalINFO("Tabelas criadas e Dados inseridos no Banco: '" + NOME_DATABASE + "'.");
+
+        } catch (SQLException e) {
+            menu.msgTerminalERROR(e.getMessage());
+        }
+    }
+    
+
+    // Excluí as Tabelas informadas no arquivo drop_tabelas.sql
+    public static void excluirTabelas () {
+        try {
+
+            String comandoSQL = arq.lerSQL(CAMINHO_PASTA_SQL + "\\" + ARQ_DROP);
+            executarSQL(comandoSQL);
+            menu.msgTerminalINFO("Tabelas anteriores exluídas do Banco: '" + NOME_DATABASE + "'.");
+
+        } catch (Exception e) {
+            menu.msgTerminalERROR("Erro ao Excluir as tabelas presentes no Bancos: '" + NOME_DATABASE + "'.");
+        }
+    }
+
+
+    // Método para executar um comando SQL
+    private static void executarSQL(String sql) {
+        try (Connection conn = conectar();
+             Statement stmt = conn.createStatement()) {
+            stmt.executeUpdate(sql);
+
+        } catch (SQLException e) {
+            menu.msgTerminalERROR("Ao realizar a execução do comando SQL: " + e.getMessage());
+
+        }
+    }
+
+    // Main
+    public static void main(String[] args) {
+        // Teste da classe
+
+        // droparDatabase();
+        // excluirTabelas();
+        
+        Connection conexao = conectar();
+        if (conexao == null) {
+            if(criarDatabase()) {
+                conectar();
+                inicializarDatabase();
+
+            }
+
+        } else {
+            inicializarDatabase();
+
+        } 
+    }
+}
